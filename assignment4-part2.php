a21d3b5eb9ef98a7c0b0d2e1c636d4858dcf3467
wangxis <wangxis@onid.oregonstate.edu>
2015-02-12 00:22:22 -0800

    done

--- assignment4-part2.php
+++ assignment4-part2.php
@@ -1,22 +1,17 @@
 <!DOCTYPE html>
 <html>
-<body>
-
-
-<form action="assignment4-part2.php" method="POST">
-<fieldset>
-<legend>Add Video</legend>
-<p>Name: <input type="text" name="videoName" ></p>
-<p>Category:<input type="text" name="videoCategory" > </p>
-<p>Length: <input type="number" name="length" min=0></p>
-<input type="submit" value="submit" name="addVideo">
-</fieldset>
-</form>
-
-
-</body>
-
-
+	<body>
+	<h3>Video Inventory</h3>
+	<form action="assignment4-part2.php" method="POST">
+		<fieldset>
+		<legend>Add Video</legend>
+			<p>Name: <input type="text" name="videoName" ></p>
+			<p>Category:<input type="text" name="videoCategory" > </p>
+			<p>Length: <input type="number" name="length" min=0></p>
+			<input type="submit" value="submit" name="addVideo">
+		</fieldset>
+		</form>
+	</body>
 </html>
 
 <?php
@@ -25,51 +20,31 @@ include 'storedInfo.php';
 
 $mysqli = new mysqli("oniddb.cws.oregonstate.edu", "wangxis-db", "$myPassword", "wangxis-db");
 if ($mysqli->connect_errno) {
-    echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
-} else {
-	//echo "Connection works!<br>";
-}
-
-/* if(isset($_POST['input'])){
-	echo 'name is set';
-	echo $_POST['input'];
-}else{
-	echo 'name is not set';
-} */
-if(isset($_POST['dropDown'])){
-	
-	//echo $_POST['checkInOutItem'];
-	echo $_POST['dropDown'];
-}else{
-	echo 'name is not set';
-}	
-
+	echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+} 
 
 /* if($mysqli->query("CREATE TABLE video_inventory(id INT PRIMARY KEY NOT NULL AUTO_INCREMENT, name VARCHAR(255) NOT NULL UNIQUE, category VARCHAR(255), length INT UNSIGNED, rented BOOL NOT NULL DEFAULT FALSE)") === TRUE) {
 	printf("Table created.\n");
 } */
+
 //delete video
-if(isset($_POST['deleteVideo'])){
-	echo $_POST['title'];
-	$title = $_POST['title'];
+if(isset($_POST['deleteVideo'])){	
 	if (!($stmt = $mysqli->prepare("DELETE FROM video_inventory WHERE name=(?)"))) {
-			echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
-		}
-		if (!$stmt->bind_param("s", $title)) {
-			echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
-		}
-		if (!$stmt->execute()) {
-			echo "Execute failed: (" . $mysqli->errno . ") " . $mysqli->error;
-		}
-		$stmt->close();
+		echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+	}
+	if (!$stmt->bind_param("s", $_POST['title'])) {
+		echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+	}
+	if (!$stmt->execute()) {
+		echo "Execute failed: (" . $mysqli->errno . ") " . $mysqli->error;
+	}
+	$stmt->close();
 }
 
 //update availability
 if(isset($_POST['availabilityUpdate'])){
-	echo $_POST['checkInOutItem'];
-	echo $_POST['checkInOut'];
-
-		if (!($stmt = $mysqli->prepare("UPDATE video_inventory SET rented = (?) WHERE name=(?)"))) {
+	if(isset($_POST['checkInOut'])) {
+		if(!($stmt = $mysqli->prepare("UPDATE video_inventory SET rented = (?) WHERE name=(?)"))) {
 			echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
 		}
 		if($_POST['checkInOut'] == 'in') {
@@ -83,18 +58,14 @@ if(isset($_POST['availabilityUpdate'])){
 		if (!$stmt->execute()) {
 			echo "Execute failed: (" . $mysqli->errno . ") " . $mysqli->error;
 		}
-
 		$stmt->close();
-
+	}
 }
-
-
 
 //add video
 if(isset($_POST['addVideo'])){
 	if(empty($_POST['videoName'])){
 		echo "Video name can not be empty <br>";
-		//echo ('<script type="text/javascript">alert ("video name is empty");</script>');
 	} else {
 		if (!($stmt = $mysqli->prepare("SELECT name FROM video_inventory"))) {
 			echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
@@ -109,7 +80,7 @@ if(isset($_POST['addVideo'])){
 		}
 		while ($stmt->fetch()){
 			if($out_name == $_POST['videoName']){
-				echo  $_POST['videoName'] . "is already in the list";
+				echo  $_POST['videoName'] . " is already in the list<br>";
 				$sameName=true;
 				break;
 			}
@@ -119,38 +90,32 @@ if(isset($_POST['addVideo'])){
 			$name = $_POST['videoName'];
 		}
 	}
-	
+	if (empty($_POST['videoCategory'])){
+		$cat="n/a";
+	} else {
 		$cat=$_POST['videoCategory'];
-		//echo $cat[0];
-	
+	}
 	if($_POST['length'] <= 0){
 		echo "The length has to be positive integer<br>";
 	} else {
 		$leng=$_POST['length'];
 	}
-	
-	if (!($stmt = $mysqli->prepare("INSERT INTO video_inventory (name, category, length) VALUES (?, ?, ?)"))) {
-    echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
-}
-	
 	if(isset($name, $cat, $leng)){
+		if (!($stmt = $mysqli->prepare("INSERT INTO video_inventory (name, category, length) VALUES (?, ?, ?)"))) {
+			echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+		}
 		if (!$stmt->bind_param("ssi", $name, $cat, $leng)) {
 			echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
-	}
-	if (!$stmt->execute()) {
-		echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
-	}
-	$stmt->close();
-		echo "all set";
-	}else {
-		echo "something is not right";
+		}
+		if (!$stmt->execute()) {
+			echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+		}
+		$stmt->close();
 	}
 }
-
 
 //Display by category
 if(isset($_POST['displayByCat'])){
-	echo $_POST['choice'];
 	if ($_POST['choice'] == 'allMovie') {
 		if (!($stmt = $mysqli->prepare("SELECT name, category, length, rented FROM video_inventory"))) {
 			echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
@@ -163,75 +128,61 @@ if(isset($_POST['displayByCat'])){
 			echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error;
 		}
 	}
-		
 }
-	if(isset($_POST['deleteAllVideo'])){
-		echo "in side";
-		if (!($stmt = $mysqli->prepare("DELETE FROM video_inventory"))) {
+
+//delete all videos
+if(isset($_POST['deleteAllVideo'])){
+	if (!($stmt = $mysqli->prepare("DELETE FROM video_inventory"))) {
+		echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+	}
+	if (!$stmt->execute()) {
+		echo "Execute failed: (" . $mysqli->errno . ") " . $mysqli->error;
+	}
+	$stmt->close();
+	echo '<h3>' . "Video List" . '</h3>';
+	echo '<table border="1">';
+	echo '<tr><td>Name</td><td>Category</td><td>Length</td><td>Availability</td><td></td><td></td></tr>';
+	echo '</table>';
+} else {
+	//display list
+	if(!isset($_POST['displayByCat']) && !isset($_POST['deleteAllVideo'])){
+		if (!($stmt = $mysqli->prepare("SELECT name, category, length, rented FROM video_inventory"))) {
 			echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
 		}
-		if (!$stmt->execute()) {
-		echo "Execute failed: (" . $mysqli->errno . ") " . $mysqli->error;
-		}
-		echo "in side";
-		$stmt->close();
-		echo "in side";
-		 echo '<table border="1"><tr>
-		<td>Name</td><td>Category</td><td>Length</td><td>Availability</td><td></td><td></td></tr>';
-		echo '</table>';
-		 
-		
-
-} else {
-
-
-
-//display list
-if(!isset($_POST['displayByCat']) && !isset($_POST['deleteAllVideo'])){
-	if (!($stmt = $mysqli->prepare("SELECT name, category, length, rented FROM video_inventory"))) {
-    echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
-}
-}
-
-if (!$stmt->execute()) {
-    echo "Execute failed: (" . $mysqli->errno . ") " . $mysqli->error;
-}
-$out_name    = NULL;
-$out_cat = NULL;
-$out_leng =NULL;
-$out_rented= NULL;
-if (!$stmt->bind_result($out_name, $out_cat, $out_leng, $out_rented)) {
-    echo "Binding output parameters failed: (" . $stmt->errno . ") " . $stmt->error;
-}
-  echo '<table border="1"><tr>
-  <td>Name</td><td>Category</td><td>Length</td><td>Availability</td><td></td><td></td></tr>';
-	while ($stmt->fetch()) {
-	if ($out_rented == 'false') {
-		$out_rented = 'Available';
-	}else {
-		$out_rented = 'Checked out';
 	}
-		echo '<tr><td>' . $out_name;
-		echo '<td>' . $out_cat . '<td>' . $out_leng . '<td>' . $out_rented;
-		
-		echo '<td>';
-		echo '<form method="POST" action="assignment4-part2.php">';
-		echo "Check-" . "in" . '<input type="radio" name="checkInOut" checked value=' . "in" . '>';
+	if (!$stmt->execute()) {
+		echo "Execute failed: (" . $mysqli->errno . ") " . $mysqli->error;
+	}
+	$out_name    = NULL;
+	$out_cat = NULL;
+	$out_leng =NULL;
+	$out_rented= NULL;
+	if (!$stmt->bind_result($out_name, $out_cat, $out_leng, $out_rented)) {
+		echo "Binding output parameters failed: (" . $stmt->errno . ") " . $stmt->error;
+	}
+	echo '<h3>' . "Video List" . '</h3>';
+	echo '<table border="1">';
+	echo '<tr><td>Name</td><td>Category</td><td>Length</td><td>Availability</td><td></td><td></td></tr>';
+	while ($stmt->fetch()) {
+		if ($out_rented == 'false') {
+			$out_rented = 'Available';
+		}else {
+			$out_rented = 'Checked out';
+		}
+		echo '<tr><td>' . $out_name . '<td>' . $out_cat . '<td>' . $out_leng . '<td>' . $out_rented;
+		echo '<td><form method="POST" action="assignment4-part2.php">';
+		echo "Check-" . "in" . '<input type="radio" name="checkInOut" value=' . "in" . '>';
 		echo "out" . '<input type="radio" name="checkInOut" value=' . "out" . '>';
 		echo '<input type="hidden" name="checkInOutItem" value=' . $out_name . '>';
-		echo '<input type="submit" value="submit" name=' . "availabilityUpdate" . '></form>';
-		echo '</td>';
-		
-		echo '<td>';
-		echo '<form method="POST" action="assignment4-part2.php">';
+		echo '<input type="submit" value="Update" name=' . "availabilityUpdate" . '></form></td>';
+		echo '<td><form method="POST" action="assignment4-part2.php">';
 		echo '<input type="hidden" name="title" value=' . $out_name;
-		echo '><input type="submit" value="Delete" name=' . "deleteVideo" . '></form>';
-		echo '</td>';
+		echo '><input type="submit" value="Delete Video" name=' . "deleteVideo" . '></form></td>';
 	}
-		
-  echo '</table>';
+	 echo '</table>';
 	$stmt->close();
 }
+//display category
 if (!($stmt = $mysqli->prepare("SELECT DISTINCT category FROM video_inventory"))) {
     echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
 }
@@ -242,29 +193,20 @@ $out_category=NULL;
 if (!$stmt->bind_result($out_category)) {
     echo "Binding output parameters failed: (" . $stmt->errno . ") " . $stmt->error;
 }
+echo '<h3>' . "Display Videos by Category " . '</h3>';
+echo '<form method="POST" action="assignment4-part2.php">';
+echo '<select name=' . "choice" . '>';
+while ($stmt->fetch()){
+	echo '<option value=' . $out_category . '>' . $out_category . '</option>';
+}
+echo '<option selected value=' . "allMovie" . '>' . "all movies" . '</option>';
+echo '<input type="submit" value="submit" name=' . "displayByCat" . '>';
+echo '</select></form>';
 
-  echo '<h3>' . "Display Videos by Category " . '</h3>';
-	echo '<form method="POST" action="assignment4-part2.php">';
-	echo '<select name=' . "choice" . '>';
-	while ($stmt->fetch()){
-		echo '<option value=' . $out_category . '>' . $out_category . '</option>';
-	}
-		
-		echo '<option selected value=' . "allMovie" . '>' . "all movies" . '</option>';
-		
-		echo '<input type="submit" value="submit" name=' . "displayByCat" . '>';
-		echo '</select>';
-echo '</form>';
-
+//delete button
 echo '<form method="POST" action="assignment4-part2.php">';
 echo '<br>';
-	echo '<input type="submit" value="Delete All Videos" name=' . "deleteAllVideo" . '>';
+echo '<input type="submit" value="Delete All Videos" name=' . "deleteAllVideo" . '>';
 echo '</form>';
-
-
-
-
-
-
 
 ?>
